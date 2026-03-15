351ee90c4dca135d971cf60e2bdf725e194f5c8e
huntlovan <hunter.lovan36@gmail.com>
2026-03-15 13:06:08 -0500

    Add fetchData function using Fetch API
    Implement fetchData function to fetch country data.

---
+++ 5-1_Demonstrate_Fetch_API/js/api.js
@@ -0,0 +1,12 @@
+export async function fetchData() {
+  // Fetch data using the Fetch API
+  // Per the documentation at https://restcountries.com/#endpoints-name
+  //   use this as the URL https://restcountries.com/v3.1/name/{name}
+  // In the above URL, substitute the value from the input for {name}
+  //   ex. The user enters "united" (without quotes)
+  //       Therefore the url becomes https://restcountries.com/v3.1/name/united
+  // Be sure to handle errors
+  //   This does not mean updating the UI with error messages
+  //   Instead, throw() an error that can be captured in the calling code
+  // Return the data to the calling function as a JavaScript object
+}
